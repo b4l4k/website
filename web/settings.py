b5022aadd756fca680352244c1d16f0224f08e51
Victor Balaniouk <balaniouk@gmail.com>
2026-02-17 20:43:53 +0100

    Set a favicon for the website, fix ALLOWED_HOSTS configuration and environment variables

--- web/settings.py
+++ web/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-development-key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')]
 
 
 # Application definition
